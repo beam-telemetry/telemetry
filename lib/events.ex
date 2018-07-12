3a64d7856c249d460198bce169e7f27dff4f6b42
Arkadiusz Gil <arkadiusz@arkgil.net>
2018-07-12 11:38:32 +0200

    Write tests and main API boilerplate

--- lib/events.ex
+++ lib/events.ex
@@ -1,3 +1,61 @@
 defmodule Events do
-  @moduledoc false
+  @moduledoc """
+  Events allows to subscribe functions to events, and call these functions when the events are
+  emitted.
+
+  Note that all subscribed functions are called in the process which emits the event.
+  """
+
+  @type subscription_id :: term()
+  @type event_name :: list()
+  @type event_prefix :: list()
+
+  @doc """
+  Creates a subscription to an event.
+
+  `subscription_id` must be unique, if another subscription with the same ID already exists the
+  `:error` atom is be returned.
+
+  When events with `event_prefix` are emitted, function `function` in module `module` will be
+  called with three arguments:
+  * the exact event name (see `emit/2`)
+  * the event value (see `emit/2`)
+  * the subscription configuration, `config`, or an empty map if one wasn't provided
+
+  If the function fails (raises, exits or throws) then the subscription is removed.
+  """
+  @spec subscribe(subscription_id, event_prefix, module, function :: atom) :: :ok | :error
+  @spec subscribe(subscription_id, event_prefix, module, function :: atom, config :: map) ::
+          :ok | :error
+  def subscribe(sub_id, event_prefix, module, function, config \\ %{}) do
+  end
+
+  @doc """
+  Removes existing subscription.
+
+  If the subscription doesn't exist, `:error` is returned.
+  """
+  @spec unsubscribe(subscription_id) :: :ok | :error
+  def unsubscribe(sub_id) do
+  end
+
+  @doc """
+  Emits an event with given name and value.
+
+  All functions subscribed to the prefix of the `event_name` will be called. Note that you should
+  not rely on the order in which those functions are called.
+  """
+  @spec emit(event_name, value :: term()) :: :ok
+  def emit(event_name, value) do
+  end
+
+  @doc """
+  Returns all subscriptions to events with given prefix.
+
+  Note that you can list all subscriptions by feeding this function an empty list.
+  """
+  @spec list_subscriptions(event_prefix) ::
+          {subscription_id, event_prefix, module, function :: atom, config :: map}
+  def list_subscriptions(event_prefix) do
+  end
 end
--- test/events_test.exs
+++ test/events_test.exs
@@ -1,3 +1,120 @@
 defmodule EventsTest do
   use ExUnit.Case
+
+  defmodule TestSubscriber do
+    def echo_event(event, value, %{send_to: pid} = config) do
+      send(pid, {:event, event, value, config})
+    end
+
+    def raise_on_event(_event, _value, _config) do
+      raise "Got an event"
+    end
+  end
+
+  setup do
+    subscription_id = :crypto.strong_rand_bytes(16) |> Base.encode16()
+
+    on_exit fn ->
+      Events.unsubscribe(subscription_id)
+    end
+
+    {:ok, sub_id: subscription_id}
+  end
+
+  test "subscribing returns error if subscription with the same ID already exist", %{
+    sub_id: sub_id
+  } do
+    :ok = Events.subscribe(sub_id, [:some, :event], TestSubscriber, :echo_event)
+
+    assert :error = Events.subscribe(sub_id, [:some, :event], TestSubscriber, :echo_event)
+  end
+
+  test "subscribed mf is called when event exactly matches the subscription prefix", %{
+    sub_id: sub_id
+  } do
+    event = [:a, :test, :event]
+    config = %{send_to: self()}
+    value = 1
+    Events.subscribe(sub_id, event, TestSubscriber, :echo_event, config)
+
+    Events.emit(event, value)
+
+    assert_receive {:event, ^event, ^value, ^config}
+  end
+
+  test "subscribed mf is not called when event is a prefix of the subscription prefix", %{
+    sub_id: sub_id
+  } do
+    event = [:a, :test]
+    prefix = event ++ [:prefix]
+    config = %{send_to: self()}
+    value = 1
+    Events.subscribe(sub_id, prefix, TestSubscriber, :echo_event, config)
+
+    Events.emit(event, value)
+
+    assert {:messages, []} == Process.info(self(), :messages)
+  end
+
+  test "mf is unsubsribed when it fails", %{sub_id: sub_id} do
+    event = [:a, :test, :event]
+    Events.subscribe(sub_id, event, TestSubscriber, :raise_on_event)
+
+    Events.emit(event, 1)
+
+    assert [] == Events.list_subscriptions(event)
+  end
+
+  test "unsubcribed mf is not called when event is emitted", %{sub_id: sub_id} do
+    event = [:a, :test, :event]
+    Events.subscribe(sub_id, event, TestSubscriber, :echo_event, %{send_to: self()})
+
+    Events.unsubscribe(sub_id)
+    Events.emit(event, 1)
+
+    assert {:messages, []} == Process.info(self(), :messages)
+  end
+
+  test "unsubscribing returns error if subscription doesn't exist", %{
+    sub_id: sub_id
+  } do
+    assert :error = Events.unsubscribe(sub_id)
+  end
+
+  test "mf subscribed to event prefix is called when event is emitted", %{sub_id: sub_id} do
+    prefix = [:a, :test]
+    event = prefix ++ [:event]
+    config = %{send_to: self()}
+    value = 1
+    Events.subscribe(sub_id, prefix, TestSubscriber, :echo_event, config)
+
+    Events.emit(event, value)
+
+    assert_receive {:event, ^event, ^value, ^config}
+  end
+
+  test "subscriptions to event can be listed", %{sub_id: sub_id} do
+    event = [:a, :test, :event]
+    config = %{send_to: self()}
+    Events.subscribe(sub_id, event, TestSubscriber, :echo_event, config)
+
+    assert [{sub_id, event, TestSubscriber, :echo_event, config}] ==
+             Events.list_subscriptions(event)
+  end
+
+  test "subscriptions to event prefix can be listed", %{sub_id: sub_id} do
+    prefix1 = []
+    prefix2 = prefix1 ++ [:a]
+    prefix3 = prefix2 ++ [:test]
+    event = prefix3 ++ [:event]
+    config = %{send_to: self()}
+    Events.subscribe(sub_id, event, TestSubscriber, :echo_event, config)
+
+    for prefix <- [prefix1, prefix2, prefix3] do
+      assert [{sub_id, event, TestSubscriber, :echo_event, config}] ==
+               Events.list_subscriptions(prefix)
+    end
+
+    assert [] == Events.list_subscriptions(event ++ [:something])
+  end
 end
