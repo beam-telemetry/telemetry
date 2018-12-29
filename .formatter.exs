debf33ef623733dccc3edaa9fe9e42e33eb98caa
Tristan Sloughter <t@crashfast.com>
2018-12-29 16:02:16 -0700

    Convert to Erlang (#28)
    * initial Erlang commit
    
    * implement telemetry functionality based on elixir version
    
    * add tests, CI and update readme
    
    * update .app.src
    
    * return map from list_handlers
    
    * updates per José's review
    
    * test on 18+ otps
    
    * make table handler docs private
    
    * fix code quoting in edoc
    
    * update readme: use :telemetry in elixir examples
    
    * use macro for stacktrace to support earlier versions
    
    * remove generated comments
    
    * rename handler_id to id
    
    * support OTP-18
    
    * rename telemetry_table_handler to telemetry_handler_table
    
    * remove state record from handler table process
    
    * set preprocess option to true for edoc
    
    * make docs for _app and _sup private

--- .formatter.exs
+++
@@ -1,7 +0,0 @@
-# Used by "mix format"
-[
-  locals_without_parens: [
-    on_exit: 1
-  ],
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
-]
---
+++ src/telemetry.hrl
@@ -0,0 +1,10 @@
+-record(handler, {id :: telemetry:handler_id(),
+                  event_name :: telemetry:event_name() | '_',
+                  function   :: telemetry:handler_function(),
+                  config     :: term() | '_'}).
+
+-ifdef('OTP_RELEASE').
+-define(WITH_STACKTRACE(T, R, S), T:R:S ->).
+-else.
+-define(WITH_STACKTRACE(T, R, S), T:R -> S = erlang:get_stacktrace(),).
+-endif.
