0b703ae415d470867a4779e87d90d4c06512a3e8
Bryan Naegele <bryannaegele@users.noreply.github.com>
2021-04-02 12:09:28 -0600

    Update src/telemetry.erl
    Co-authored-by: José Valim <jose.valim@plataformatec.com.br>

--- src/telemetry.erl
+++ src/telemetry.erl
@@ -160,7 +160,7 @@ execute(EventName, Measurements, Metadata) when is_map(Measurements) and is_map(
 %% the user in the `StartMetadata`. This context is useful for tracing libraries to identify unique
 %% executions of span events within a process to match start, stop, and exception events. Users
 %% should ensure this value is unique within the context of a process at a minimum if overriding this key and
-%% that the same value is provided to bot `StartMetadata` and `StopMetadata`.
+%% that the same value is provided to both `StartMetadata` and `StopMetadata`.
 %%
 %% For `telemetry' events denoting the <strong>start</strong> of a larger event, the following data is provided:
 %% <p>
