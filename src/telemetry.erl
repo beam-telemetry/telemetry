7b99309898cc827fda3f1956e23d34d349453f16
Andrea Leopardi <an.leopardi@gmail.com>
2025-01-15 09:04:17 +0100

    Fix the telemetry:span_function/0 type (#137)

--- src/telemetry.erl
+++ src/telemetry.erl
@@ -29,7 +29,7 @@ and `execute/2`.
 -type handler_config() :: term().
 -type handler_function() :: fun((event_name(), event_measurements(), event_metadata(), handler_config()) -> any()).
 -type span_result() :: term().
--type span_function() :: fun(() -> {span_result(), event_metadata()}) | {span_result(), event_measurements(), event_metadata()}.
+-type span_function() :: fun(() -> {span_result(), event_metadata()} | {span_result(), event_measurements(), event_metadata()}).
 -type handler() :: #{id := handler_id(),
                      event_name := event_name(),
                      function := handler_function(),
